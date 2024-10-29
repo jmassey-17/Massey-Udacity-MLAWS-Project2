d7fa240ad7f44fcadf976625ac2bc7015b0d740d
jmassey-17 <jamie435@hotmail.co.uk>
2024-10-29 13:34:22 +0100

    Second Commit 20241029

--- lambda_function2.py
+++ lambda_function2.py
@@ -5,27 +5,25 @@ import base64
 import boto3
 
 # Fill this in with the name of your deployed model
-ENDPOINT ='image-classification-2024-10-29-08-00-59-869'
+ENDPOINT ='image-classification-2024-10-29-11-26-09-541'
 
 def lambda_handler(event, context):
 
     # Decode the image data
     image = base64.b64decode(event['body']['image_data'])
-    
     # Define content
     content_type = "image/png"
-    
-    # Initialize SageMaker runtime client
+    # Initialize the SageMaker runtime client
     sagemaker_runtime = boto3.client("sagemaker-runtime")
 
-    # Make prediction 
+    # Make the prediction request
     inferences = sagemaker_runtime.invoke_endpoint(
         EndpointName=ENDPOINT,
         ContentType=content_type,
         Body=image
     )
 
-    # upload inferences 
+    # We return the data back to the Step Function    
     event["inferences"] = json.loads(inferences['Body'].read().decode('utf-8'))
     return {
         'statusCode': 200,
